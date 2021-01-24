678d64209ac4323f4d037d5e78fb348cf846a79f
Nakula Marvellio <nakulamarvellio@gmail.com>
2021-01-24 23:22:20 +0700

    Get HTML Elements to ScriptJS

---
+++ 11 -Custom HMTL5 Video Player/script.js
@@ -0,0 +1,12 @@
+// get our elements
+const player = document.querySelector(".palyer");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+
+// build out functions
+
+//  Hook up the event listeners
