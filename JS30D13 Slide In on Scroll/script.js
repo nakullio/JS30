64260bf220fac53c4d2dff3014c04c9d2ee812ca
Nakula Marvellio <nakulamarvellio@gmail.com>
2021-01-28 03:52:38 +0700

    add sliderImage effect based on scrolls

--- JS30D13 Slide In on Scroll/script.js
+++ JS30D13 Slide In on Scroll/script.js
@@ -1,4 +1,4 @@
-function debounce(func, wait = 20, immediate = true) {
+function debounce(func, wait = 10, immediate = true) {
   var timeout;
   return function () {
     var context = this,
@@ -17,8 +17,25 @@ function debounce(func, wait = 20, immediate = true) {
 // select the picture we want to open.
 const sliderImages = document.querySelectorAll(".slide-in");
 
-function checkSlide(e) {
-  console.count(e);
+function checkSlide() {
+  // loop over every single images
+  sliderImages.forEach((sliderImage) => {
+    // to get the pixel level where we at the bottom of the scroll
+    // this is the half way through the image
+    const slideInAt =
+      window.scrollY + window.innerHeight - sliderImage.height / 2;
+    // check the px of bottom image position
+    // this is the bottom of the image
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.scollY < imageBottom;
+    // if statement for ifhalfshown & isnotscrolledpast
+    if (isHalfShown && isNotScrolledPast) {
+      sliderImage.classList.add("active");
+    } else {
+      sliderImage.classList.remove("active");
+    }
+  });
 }
 
-window.addEventListener("scroll", checkSlide);
+window.addEventListener("scroll", debounce(checkSlide));
