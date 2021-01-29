4a0e28fd6101e4b9dbe6b248bb2f852e423806cf
Nakula Marvellio <nakulamarvellio@gmail.com>
2021-01-30 01:12:11 +0700

    add listener to localStorgae and update for every changes

--- JS30D15 LocalStorage and Event Delegation/script.js
+++ JS30D15 LocalStorage and Event Delegation/script.js
@@ -36,7 +36,17 @@ function populateList(plates = [], platesList) {
     .join("");
 }
 
-addItems.addEventListener("submit", addItem);
+function toggleDone(e) {
+  if (!e.target.matches("input")) return; // skip this unless it's an input
+  //   update actual changes on input item list
+  const el = e.target;
+  const index = el.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItem("items", JSON.stringify(items));
+  populateList(items, itemsList);
+}
 
+addItems.addEventListener("submit", addItem);
+itemsList.addEventListener("click", toggleDone);
 // once page load
 populateList(items, itemsList);
