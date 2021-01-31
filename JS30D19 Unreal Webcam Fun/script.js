42b3c45266db154b47c9b35d556aaf151ffe3a43
Nakula Marvellio <nakulamarvellio@gmail.com>
2021-02-01 06:52:35 +0700

    add getVideo to access the web camera and run as local

--- JS30D19 Unreal Webcam Fun/script.js
+++ JS30D19 Unreal Webcam Fun/script.js
@@ -3,3 +3,29 @@ const canvas = document.querySelector(".photo");
 const ctx = canvas.getContext("2d");
 const strip = document.querySelector(".strip");
 const snap = document.querySelector(".snap");
+
+function getVideo() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then((localMediaStream) => {
+      console.log(localMediaStream);
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    })
+    .catch((err) => {
+      console.error(`OH NOO!!!`, err);
+    });
+}
+
+function paintToCnvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 16);
+}
+
+getVideo();
