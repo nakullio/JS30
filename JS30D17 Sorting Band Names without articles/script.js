96f98fee1df303aa0a1bd152af729f21e3956c97
Nakula Marvellio <nakulamarvellio@gmail.com>
2021-02-01 04:30:33 +0700

    add sorting function with regex and mapping out to HTML structure

--- JS30D17 Sorting Band Names without articles/script.js
+++ JS30D17 Sorting Band Names without articles/script.js
@@ -13,3 +13,17 @@ const bands = [
   "Anywhere But Here",
   "An Old Dog",
 ];
+
+function strip(bandName) {
+  // return and replace with regex (regular expression)
+  return bandName.replace(/^(a |the |an )/i, "").trim();
+}
+
+const sortedBands = bands.sort((a, b) => (strip(a) > strip(b) ? 1 : -1));
+
+//  wrap our bands with sorted and loop over the list
+document.querySelector("#bands").innerHTML = sortedBands
+  .map((band) => `<li>${band}</li>`)
+  .join("");
+
+console.log(sortedBands);
