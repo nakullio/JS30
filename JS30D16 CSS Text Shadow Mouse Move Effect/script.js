faeee244a9ca140371fc732168219592df03cb0b
Nakula Marvellio <nakulamarvellio@gmail.com>
2021-02-01 03:59:46 +0700

    add style on textShadow for each move positions

--- JS30D16 CSS Text Shadow Mouse Move Effect/script.js
+++ JS30D16 CSS Text Shadow Mouse Move Effect/script.js
@@ -1,6 +1,7 @@
 // grab the hero calss and text
 const hero = document.querySelector(".hero");
 const text = hero.querySelector("h1");
+const walk = 500; // 100px
 
 function shadow(e) {
   //   get the width and the height
@@ -15,7 +16,15 @@ function shadow(e) {
     x = x + e.target.offsetLeft;
     y = y + e.target.offsetTop;
   }
-  console.log(x, y);
+  const xWalk = Math.round((x / width) * walk - walk / 2);
+  const yWalk = Math.round((y / height) * walk - walk / 2);
+
+  text.style.textShadow = `
+  ${xWalk}px ${yWalk}px 0 rgba(225,0,255, 0.7),
+  ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,255, 0.7),
+  ${yWalk}px ${xWalk * -1}px 0 rgba(0,255,0, 0.7),
+  ${yWalk * -1}px ${xWalk}px 0 rgba(0,0,255, 0.7) 
+  `;
 }
 
 hero.addEventListener("mousemove", shadow);
