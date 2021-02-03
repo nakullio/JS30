5de2c75a86dc16c4b1e4a9ab583b5b48a7f7b118
Nakula Marvellio <nakulamarvellio@gmail.com>
2021-02-03 17:10:11 +0700

    add speech Recognition listener

--- JS30D20 Native Speech Recognition/script.js
+++ JS30D20 Native Speech Recognition/script.js
@@ -1,2 +1,18 @@
 window.SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement("p");
+const words = document.querySelector(".words");
+words.appendChild(p);
+
+recognition.addEventListener("result", (e) => {
+  console.log(e.results);
+  const transcript = Array.from(e.results).map((result) => result[0]);
+
+  console.log(transcript);
+});
+
+recognition.start();
