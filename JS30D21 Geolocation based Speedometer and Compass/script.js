d147610be18f7dd22812527bb2dc3e6ce7b40072
Nakula Marvellio <nakulamarvellio@gmail.com>
2021-02-04 20:56:25 +0700

    get arrow and speed based on geoloc data

---
+++ JS30D21 Geolocation based Speedometer and Compass/script.js
@@ -0,0 +1,15 @@
+// get the arrow and speed from html
+const arrow = document.querySelector(".arrow");
+const speed = document.querySelector(".speed-value");
+
+// set the position live
+navigator.geolocation.watchPosition(
+  (data) => {
+    console.log(data);
+    speed.textContent = data.coords.speed;
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+  },
+  (err) => {
+    console.error(err);
+  }
+);
