3ec5e08eee503a71f8ade3f44ecd94dbe3f3366e
Nakula Marvellio <nakulamarvellio@gmail.com>
2021-02-05 21:56:46 +0700

    aading class of highlight to an active mouse pointer/pict>

---
+++ JS30D22 Follow Along Links/script.js
@@ -0,0 +1,16 @@
+// 👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀
+
+// get all the link element
+const triggers = document.querySelectorAll("a");
+const highlight = document.createElement("span");
+
+// add a class of highlight
+highlight.classList.add("highlight");
+document.body.appendChild(highlight);
+
+function highlightLink()
+ {
+     console.log('highlight!!')
+ }
+
+ triggers,foreEach (add.evenLetenr => a)
