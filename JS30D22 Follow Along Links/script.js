db0d540e57385252dd7ef45c986d8c83dbb0bcb6
Nakula Marvellio <nakulamarvellio@gmail.com>
2021-02-07 00:22:02 +0700

    add precision coords with scrollY & scrollX changes

--- JS30D22 Follow Along Links/script.js
+++ JS30D22 Follow Along Links/script.js
@@ -11,9 +11,18 @@ document.body.appendChild(highlight);
 function highlightLink() {
   const linkCoords = this.getBoundingClientRect();
   console.log(linkCoords);
-  highlight.style.width = `${linkCoords.width}px`;
-  highlight.style.height = `${linkCoords.height}px`;
-  highlight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top}px)`;
+
+  //   record the scroll postion for precision
+  const coords = {
+    width: linkCoords.width,
+    height: linkCoords.height,
+    top: linkCoords.top + window.scrollY,
+    left: linkCoords.left + window.scrollX,
+  };
+
+  highlight.style.width = `${coords.width}px`;
+  highlight.style.height = `${coords.height}px`;
+  highlight.style.transform = `translate(${coords.left}px, ${linkCoords.top}px)`;
 }
 
 triggers.forEach((a) => a.addEventListener("mouseenter", highlightLink));
