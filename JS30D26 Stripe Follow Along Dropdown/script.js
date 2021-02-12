39cd846a96bbcf5e67829737bfe52c1c2b743aca
Nakula Marvellio <nakulamarvellio@gmail.com>
2021-02-12 23:29:57 +0700

    add handle listener on mouse enter & leave

--- JS30D26 Stripe Follow Along Dropdown/script.js
+++ JS30D26 Stripe Follow Along Dropdown/script.js
@@ -2,3 +2,21 @@
 const triggers = document.querySelectorAll(".cool > li");
 const background = document.querySelector(".dropdownBackground");
 const nav = document.querySelector(".top");
+
+// listen to hover in and hover out on mouse enter & leave
+
+function handleEnter() {
+  console.log("ENTER");
+}
+
+function handleLeave() {
+  console.log("LEAVE");
+}
+
+triggers.forEach((trigger) =>
+  trigger.addEventListener("mouseenter", handleEnter)
+);
+
+triggers.forEach((trigger) =>
+  trigger.addEventListener("mouseleave", handleLeave)
+);
