b61c56874581084d2d0a3012fe3c46331da7cc04
Nakula Marvellio <nakulamarvellio@gmail.com>
2021-02-12 23:46:57 +0700

    set the styling background coordinate

--- JS30D26 Stripe Follow Along Dropdown/script.js
+++ JS30D26 Stripe Follow Along Dropdown/script.js
@@ -10,10 +10,30 @@ function handleEnter() {
   setTimeout(() => {
     this.classList.add("trigger-enter-active");
   }, 150);
+  background.classList.add("open");
+
+  //   what that dropdown actually is
+  const dropdown = this.querySelector(".dropdown");
+  console.log(dropdown);
+
+  const dropdownCoords = dropdown.getBoundingClientRect();
+  //   grab the nav coordinate
+  const navCoords = nav.getBoundingClientRect();
+
+  //   get the two coords together
+  const coords = {
+    height: dropdownCoords.height,
+    width: dropdownCoords.width,
+  };
+
+  //   styling the background
+  background.style.setProperty("width", `${coords.width}px`);
+  background.style.setProperty("width", `${coords.height}px`);
 }
 
 function handleLeave() {
   this.classList.remove("trigger-enter", "trigger-enter-active");
+  background.classList.remove("open");
 }
 
 triggers.forEach((trigger) =>
