a13a42888b6c2edf544b4bfe3aeda9f7583fb3c5
Nakula Marvellio <nakulamarvellio@gmail.com>
2021-02-15 23:51:12 +0700

    add displayEndTime

--- JS30D30 Countdown Timer/script.js
+++ JS30D30 Countdown Timer/script.js
@@ -1,5 +1,8 @@
 // store setInternval in global fun
 let countdown;
+// set the display
+const timerDisplay = document.querySelector(".display__time-left");
+const endTime = document.querySelector(".display__end-time");
 
 // create function call timer
 function timer(seconds) {
@@ -7,19 +10,37 @@ function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(then);
   //   display the timer
   countdown = setInterval(() => {
     // how much time left on the clock
-    const secondLeft = Math.round((then - Date.now()) / 1000);
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
     // check if we should stop it
-    if (secondLeft < 0) {
+    if (secondsLeft <= 0) {
       clearInterval(countdown);
     }
     // display it
-    console.log(secondLeft);
+    console.log(secondsLeft);
   }, 1000);
 }
 
 function displayTimeLeft(seconds) {
-  console.log(seconds);
+  // convert into seconds, take math.floor for the lower bound
+  const minutes = Math.floor(seconds / 60);
+  const reminderSeconds = seconds % 60;
+  //   display the timer
+  const display = `${minutes}:${
+    reminderSeconds < 10 ? "0" : ""
+  }${reminderSeconds}`;
+  document.title = display;
+  timerDisplay.textContent = display;
+}
+
+// showing the ending time
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  //   get the hours & minutes  from date object above
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back At ${hour > 12 ? hour - 12 : hour}:${minutes}`;
 }
