03fc91fd5c3f538260f0c50833e12cf8e84ea0ba
Nakula Marvellio <nakulamarvellio@gmail.com>
2021-02-15 23:02:27 +0700

    set the displayTimeLeft

--- JS30D30 Countdown Timer/script.js
+++ JS30D30 Countdown Timer/script.js
@@ -1,7 +1,25 @@
+// store setInternval in global fun
+let countdown;
+
 // create function call timer
 function timer(seconds) {
   //  when the timer start
   const now = Date.now();
   const then = now + seconds * 1000;
-  console.log({ now, then });
+  displayTimeLeft(seconds);
+  //   display the timer
+  countdown = setInterval(() => {
+    // how much time left on the clock
+    const secondLeft = Math.round((then - Date.now()) / 1000);
+    // check if we should stop it
+    if (secondLeft < 0) {
+      clearInterval(countdown);
+    }
+    // display it
+    console.log(secondLeft);
+  }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  console.log(seconds);
 }
