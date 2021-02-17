d1655e5504ea9a526a3227562687cacfee1bfc9f
Nakula Marvellio <nakulamarvellio@gmail.com>
2021-02-17 23:03:28 +0700

    set pseudocode for the js file

--- JS30D29 Whack A Mole Game/script.js
+++ JS30D29 Whack A Mole Game/script.js
@@ -1,3 +1,8 @@
 const holes = document.querySelectorAll(".hole");
 const scoreBoard = document.querySelector(".score");
 const moles = document.querySelectorAll(".mole");
+
+// set the function for interact with elements
+// listen to any events
+// check at the DOM for development and result
+// set the Global and Local Scope in appropiate way
