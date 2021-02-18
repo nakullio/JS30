b974755445398c99d9a33fe13449ba8209b5083e
Nakula Marvellio <nakulamarvellio@gmail.com>
2021-02-18 21:47:25 +0700

    set random time and hole function

--- JS30D29 Whack A Mole Game/script.js
+++ JS30D29 Whack A Mole Game/script.js
@@ -1,6 +1,7 @@
 const holes = document.querySelectorAll(".hole");
 const scoreBoard = document.querySelector(".score");
 const moles = document.querySelectorAll(".mole");
+let lastHole;
 
 // set the function for interact with elements
 // listen to any events
@@ -9,4 +10,21 @@ const moles = document.querySelectorAll(".mole");
 
 // pop up the mole randomy wihtin few seconds range
 // when mouse click on mole, it will be update the scores
-//
+
+function randTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+// pick the random hole for the mole pop up from
+function randomHole(holes) {
+  // find the index of holes
+  const idx = Math.floor(Math.random() * holes.length);
+  const hole = holes[idx];
+  if (hole === lastHole) {
+    console.log(" hahahahaha");
+    return randomHole(holes);
+  }
+
+  lastHole = hole;
+  return hole;
+}
