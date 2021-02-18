e03607f83b3a7b73d446b7a3cee26a232c0d4d23
Nakula Marvellio <nakulamarvellio@gmail.com>
2021-02-18 21:35:17 +0700

    update pseudocode

--- JS30D29 Whack A Mole Game/script.js
+++ JS30D29 Whack A Mole Game/script.js
@@ -6,3 +6,7 @@ const moles = document.querySelectorAll(".mole");
 // listen to any events
 // check at the DOM for development and result
 // set the Global and Local Scope in appropiate way
+
+// pop up the mole randomy wihtin few seconds range
+// when mouse click on mole, it will be update the scores
+//
